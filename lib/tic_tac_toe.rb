f09c097c2155a77531006235b790e889bd5da6b9
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-01-26 21:30:28 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,10 +34,10 @@ class TicTacToe
   end
 
   def position_taken?(index)
-    if @board[index] == nil
-      false
-    else
+    if @board[index] == "X" || @board[index] == "O"
       true
+    else
+      false
     end
   end
 
